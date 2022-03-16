c5b969c75e425253ecadb7927e8cbd282b5a2d47
Geetika jindal <geetikajindal78@gmail.com>
2022-03-16 15:24:46 +0530

    Updated CSS

--- src/App.js
+++ src/App.js
@@ -4,20 +4,50 @@ import './App.css';
 function App() {
   const user= [
   {
-    name: "Geetika",
+    name: "A",
     location: "India",
     foodType: "Veg",
-    age: "23",
+    age: "22",
     likes: "Coding",
-    github: "geetujindal7",
+    github: "A",
     avatar: "https://assets.materialup.com/uploads/0bee3e01-ba96-4a9d-b288-620780ba6269/attachment.png"
+
+  },
+  {
+    name: "B",
+    location: "USA",
+    foodType: "Veg",
+    age: "24",
+    likes: "Dancing",
+    github: "B",
+    avatar: "https://img.freepik.com/free-vector/smiling-girl-avatar_102172-32.jpg"
+
+  },
+  {
+    name: "C",
+    location: "Canada",
+    foodType: "Veg",
+    age: "29",
+    likes: "Eating",
+    github: "C",
+    avatar: "https://i.pinimg.com/736x/ef/b4/56/efb4563befb0ae1bed74f004785f3f0f.jpg"
+
+  },
+  {
+    name: "D",
+    location: "Paris",
+    foodType: "Veg",
+    age: "26",
+    likes: "Playing",
+    github: "D",
+    avatar: "https://img.lovepik.com/free-png/20210923/lovepik-cute-girl-avatar-png-image_401231841_wh1200.png"
 
   },
 
  ]
   return (
     <div className="App">
-     <div style={{width:"100%",display:"flex", float:"left"}}>
+     <div style={{width:"100%",display:"flex", float:"left", flexWrap:"wrap"}}>
       {
         user.map((i) =>
         {
